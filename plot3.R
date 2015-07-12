4043c612b5e7f161e6ac9482d50392b524f95329
008000 <008000@users.noreply.github.com>
2015-07-12 21:48:22 +0000

    add plot3

---
+++ plot3.R
@@ -0,0 +1,16 @@
+data <- read.csv("household_power_consumption_subset.csv", sep = ";")
+datetimeStrings <- paste(data$Date, data$Time)
+data$datetime <- strptime(datetimeStrings, "%d/%m/%Y %H:%M:%S")
+
+Sys.setlocale("LC_TIME", "English")
+
+png(filename = "plot3.png")
+with(data, {
+    plot(datetime, Sub_metering_1, type = "n", ylab = "Energy sub metering", xlab="")
+    lines(datetime, Sub_metering_1, col = "black")
+    lines(datetime, Sub_metering_2, col = "red")
+    lines(datetime, Sub_metering_3, col = "blue")
+    legend("topright", lwd = 1, col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+})
+dev.off() 
+
