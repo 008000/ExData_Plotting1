5b05ba7858c91c6ab6886a99a445ee55a37ec76f
008000 <008000@users.noreply.github.com>
2015-07-12 21:29:04 +0000

    add plot2

---
+++ plot2.R
@@ -0,0 +1,12 @@
+data <- read.csv("household_power_consumption_subset.csv", sep = ";")
+datetimeStrings <- paste(data$Date, data$Time)
+data$datetime <- strptime(datetimeStrings, "%d/%m/%Y %H:%M:%S")
+
+png(filename = "plot2.png")
+with(data, {
+    plot(datetime, Global_active_power, type = "l", ylab = "Global Active Power (kilowatts)", xlab="")
+})
+dev.off() 
+
+
+
